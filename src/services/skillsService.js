0580f9fba6277f38c922d19d62a33ee7f3236025
YousefYassan <youesfhassan256@gmail.com>
2026-04-20 17:14:14 +0200

    add Dockerfile only

--- src/services/skillsService.js
+++
@@ -1,7 +0,0 @@
-import { api } from "./api";
-
-// Get All Skills - GET /api/skills/get-skills
-export const getAllSkills = async () => {
-  const response = await api.get("/api/skills/get-skills");
-  return response.data;
-};
